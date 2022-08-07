031dddd88f2bbe55d96862f69bd01dcef17e558c
Liu Xinyi <67133123+Liuxinyiii@users.noreply.github.com>
2022-08-07 14:44:27 +0100

    Update getdata.py

--- src/getdata.py
+++ src/getdata.py
@@ -18,7 +18,7 @@ pro = ts.pro_api('***')
 
 # %%
 # # 指数成分股数据
-# pro = ts.pro_api('432455924591f5b8bc3a633340ccb0e719d7ab1edf1b939542329b50')
+# pro = ts.pro_api('***')
 # result = pd.DataFrame()
 # for year in [x for x in range(10,22)]:
 #     for month in [x for x in range(1,13)]:
@@ -26,7 +26,7 @@ pro = ts.pro_api('***')
 
 # %%
 # # 成分股日度数据
-# pro = ts.pro_api('432455924591f5b8bc3a633340ccb0e719d7ab1edf1b939542329b50')
+# pro = ts.pro_api('***')
 # idx_compo = pd.read_csv('/Users/liuxinyi/Desktop/论文/data/index_compo_data.csv')
 # for i in tqdm(range(383,len(idx_compo))):
 #     code = str(idx_compo['成分股代码_CompoStkCd'].iloc[i]).zfill(6)
@@ -40,7 +40,7 @@ pro = ts.pro_api('***')
 
 # %% 因子数据
 # auth('15201141906', 'Zhengrenlianghua2020')
-pro = ts.pro_api('432455924591f5b8bc3a633340ccb0e719d7ab1edf1b939542329b50')
+pro = ts.pro_api('***')
 
 ff3 = pd.read_csv('/Users/liuxinyi/Desktop/论文/data/ff3_0.csv',index_col=0).append(pd.read_csv('/Users/liuxinyi/Desktop/论文/data/ff3_1.csv',index_col=0))
 ff3.date = pd.to_datetime(ff3.date.apply(str))
